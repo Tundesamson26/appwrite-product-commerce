02230d5a3ec1f9d5ba524d955ef6199b458b3ac2
Tunde Samson <odewoletundesamson@gmail.com>
2023-06-14 22:04:30 +0100

    updated commit

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-export default({  
-  buildModules: [
-    '@nuxtjs/vite'
-  ],
+export default {
+  buildModules: ["@nuxtjs/vite"],
   vite: {
     css: {
-      postcss: false
-    }
+      postcss: false,
+    },
   },
+  modules: [
+    [
+      'nuxt-sweetalert2',
+      {
+        confirmButtonColor: '#FFFFFF'
+      }
+    ]
+  ],
   runtimeConfig: {
     public: {
       PROJECT_ID: process.env.NUXT_PROJECT_ID,
@@ -21,35 +27,39 @@ export default({
       TICKET_COLLECTION: process.env.NUXT_TICKET_COLLECTION_ID,
       TICKET_BUCKET_ID: process.env.NUXT_TICKET_BUCKET_ID,
       ACCOUNT_DETAILS_DB_ID: process.env.NUXT_ACCOUNT_DETAILS_DATABASE_ID,
-      ACCOUNT_DETAILS_COLLECTION: process.env.NUXT_ACCOUNT_DETAILS_COLLECTION_ID,
+      ACCOUNT_DETAILS_COLLECTION:
+        process.env.NUXT_ACCOUNT_DETAILS_COLLECTION_ID,
       SUPPORT_DATABASE_ID: process.env.NUXT_SUPPORT_DB_ID,
-      SUPPORT_COLLECTION_ID: process.env.NUXT_SUPPORT_COLLECTION_ID
+      SUPPORT_COLLECTION_ID: process.env.NUXT_SUPPORT_COLLECTION_ID,
     },
-  },  
+  },
   router: {
-    extendRoutes(routes: { path: string; component: any; }[], resolve: (arg0: string, arg1: string) => any) {
+    extendRoutes(
+      routes: { path: string; component: any }[],
+      resolve: (arg0: string, arg1: string) => any
+    ) {
       routes.push(
         {
-          path: '/dashboard',
-          component: resolve(__dirname, 'pages/dashboard.vue')
+          path: "/dashboard",
+          component: resolve(__dirname, "pages/dashboard.vue"),
         },
         {
-          path: '/wallet',
-          component: resolve(__dirname, 'pages/wallet.vue')
+          path: "/wallet",
+          component: resolve(__dirname, "pages/wallet.vue"),
         },
         {
-          path: '/products',
-          component: resolve(__dirname, 'pages/products.vue')
+          path: "/products",
+          component: resolve(__dirname, "pages/products.vue"),
         },
         {
-          path: '/customers',
-          component: resolve(__dirname, 'pages/courses.vue')
+          path: "/customers",
+          component: resolve(__dirname, "pages/courses.vue"),
         },
         {
-          path: '/sales',
-          component: resolve(__dirname, 'pages/sales.vue')
+          path: "/sales",
+          component: resolve(__dirname, "pages/sales.vue"),
         }
       );
-    }
+    },
   },
-})
+};
